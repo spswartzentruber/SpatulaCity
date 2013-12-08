b39f7450b7d0914be6736342b890071593081902
spswartzentruber <zpeterz63@gmail.com>
2013-12-08 15:25:47 -0800

    Modified links to to be relative paths instead of absolute

--- nav_bar.php
+++ nav_bar.php
@@ -1,10 +1,10 @@
 <?php session_start(); ?>
 <div id="navMenu" class="navigation">
     <ul>
-        <li class="single-menu-item"><a href="/spatulacity/index.php">About</a></li>
-        <li class="single-menu-item"><a href="/spatulacity/browse.php?page=1">Browse</a></li>
-        <li class="single-menu-item"><a href="/spatulacity/cart.php">Cart [<?php echo count($_SESSION['cart']); ?>]</a></li>
-        <li class="single-menu-item"><a href="/spatulacity/create_spatula.php">Create Spatula</a></li>
+        <li class="single-menu-item"><a href="index.php">About</a></li>
+        <li class="single-menu-item"><a href="browse.php?page=1">Browse</a></li>
+        <li class="single-menu-item"><a href="cart.php">Cart [<?php echo count($_SESSION['cart']); ?>]</a></li>
+        <li class="single-menu-item"><a href="create_spatula.php">Create Spatula</a></li>
         <?php
 		if(isset($_SESSION['username']))
 		{
