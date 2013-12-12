10a70e5d4485d090bfb6d9f8720c08c9822541d8
spswartzentruber <zpeterz63@gmail.com>
2013-12-12 05:42:50 -0800

    -changed form from a bunch of divs to a table. -Did some indenting to increase code readability.

--- create_spatula.php
+++ create_spatula.php
@@ -1,32 +1,54 @@
-<?php 
-include 'page_elements/header.php'; 
-?>
+<?php include 'page_elements/header.php'; ?>
 
 <body>
-<div id="banner">
-	<?php include 'login.php'; ?>
-</div>
-<div id="below_banner">
-<?php
-include 'page_elements/nav_bar.php';
-?>
-<div id='content'>
-<form action="form_handler.php" method="post" id="new_spatula_form">
-	<fieldset>
-        <legend>New Spatula:</legend>
-        Name: <input type="text" required name="spatula[name]"><br>
-        Manufacturer: <input type="text" required name="spatula[manufacturer]"><br>
-        Color: <input type="text" required name="spatula[color]"><br>
-        Num in stock: <input type="text" required name="spatula[inventory]"><br>
-        Image URL: <input type="text" required name="spatula[image]"><br>
-        Sale Price: <input type="number" required name="spatula[sale_price]"><br>
-        Retail Price: <input type="number" required name="spatula[retail_price]"><br>
-        Description: <input type="textarea" required name="spatula[description]"><br>
-        <input type='submit' value='submit'>
-	</fieldset>
-</form>
-</div>
-</div>
+	<div id="banner">
+		<?php include 'login.php'; ?>
+	</div>
+	<div id="below_banner">
+		<?php include 'page_elements/nav_bar.php'; ?>
+		<div id='content'>
+			<form action="form_handler.php" method="post" id="new_spatula_form">
+				<fieldset>
+					<legend>New Spatula:</legend>
+						<table>
+							<tr>
+								<td>Name:</td>
+								<td><input type="text" required name="spatula[name]"></td>
+							</tr>
+							<tr>
+								<td>Manufacturer:</td>
+								<td><input type="text" required name="spatula[manufacturer]"></td>
+							</tr>
+							<tr>
+								<td>Color:</td>
+								<td><input type="text" required name="spatula[color]"></td>
+							</tr>
+							<tr>
+								<td>Num in stock:</td>
+								<td><input type="text" required name="spatula[inventory]"></td>
+							</tr>
+							<tr>
+								<td>Image URL:</td>
+								<td><input type="text" required name="spatula[image]"></td>
+							</tr>
+							<tr>
+								<td>Sale Price:</td>
+								<td><input type="number" required name="spatula[sale_price]"></td>
+							</tr>
+							<tr>
+								<td>Retail Price:</td>
+								<td><input type="number" required name="spatula[retail_price]"></td>
+							</tr>
+							<tr>
+								<td>Description:</td>
+								<td><input type="textarea" required name="spatula[description]"></td>
+							</tr>
+						</table>
+					<input type='submit' value='submit'>
+				</fieldset>
+			</form>
+		</div>
+	</div>
 </body>
 
 <?php include 'page_elements/footer.php'; ?>
